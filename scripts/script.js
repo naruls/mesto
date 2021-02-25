3278d1e013a1f090ab5d6f7c142e31da7be573e9
Kirill Nihaenko <Kirill.nihaenko@yandex.ru>
2021-02-25 15:35:20 +0300

    Первые исправления

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,16 @@
 let container = document.querySelector('.container');
 let changeButton = container.querySelector('.profile__change-button');
 let resetButton = container.querySelector('.popup__close-button');
-let saveButton = container.querySelector('.popup__save-button');
+let saveButton = container.querySelector('.popup__form');
 let popup = container.querySelector('.popup_hidden');
 let profileName = container.querySelector('.profile__name');
 let profileDescription = container.querySelector('.profile__description');
+let name = container.querySelector('.popup__input_user_name');
+let description = container.querySelector('.popup__input_user_description');
 
 function popupShow() {
-let name = container.querySelector('.popup__name');
-let description = container.querySelector('.popup__description');
-name.value= profileName.innerHTML;
-description.value= profileDescription.innerHTML;
+name.value= profileName.textContent;
+description.value= profileDescription.textContent;
 popup.classList.add('popup_active');
 }
 
@@ -22,15 +22,14 @@ popup.classList.remove('popup_active');
 
 resetButton.addEventListener('click', popupHidde); 
 
-function popupSave() {
-let name = container.querySelector('.popup__name');
-let description = container.querySelector('.popup__description');
-container.querySelector('.profile__name').innerHTML="";
-container.querySelector('.profile__description').innerHTML="";
+function popupSave(evt) {
+evt.preventDefault();
+container.querySelector('.profile__name').textContent="";
+container.querySelector('.profile__description').textContent="";
 profileName.insertAdjacentText('beforeend', `${name.value}`);
 profileDescription.insertAdjacentText('beforeend', `${description.value}`);
 popup.classList.remove('popup_active');
 }
 
-saveButton.addEventListener('click', popupSave); 
+saveButton.addEventListener('submit', popupSave); 
 
